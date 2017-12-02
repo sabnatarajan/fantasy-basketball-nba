cb7ba1957eb6b51e730c133a8cba2d500d0e1ff9
Sabareesh Natarajan <sab.natarajan@gmail.com>
2017-12-02 01:17:12 -0500

    Fix collectstatic for deploy

--- fantasy_nba/fantasy_nba/settings.py
+++ fantasy_nba/fantasy_nba/settings.py
@@ -129,7 +129,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'client/build'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
