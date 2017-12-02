fa31181c0bccc4d93810dc319167b8e5255b1d14
Sabareesh Natarajan <sab.natarajan@gmail.com>
2017-12-02 01:25:41 -0500

    -

--- fantasy_nba/fantasy_nba/settings.py
+++ fantasy_nba/fantasy_nba/settings.py
@@ -129,7 +129,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'client/build'),
+    os.path.join(BASE_DIR, 'client/assets'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
